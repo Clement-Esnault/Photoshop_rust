4cb2e7c6a19323daddc9325db019f0bc124c3e3f
Clement <clement.esnault@etu.unicaen.fr>
2026-09-09 11:30:21 +0200

    Setup frontend Vue + rust-core WASM

---
+++ editeur_rust/frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { onMounted } from 'vue'
+import init, { add } from './wasm/rust_core.js'
+
+onMounted(async () => {
+  await init()
+  console.log(add(2, 3)) // doit afficher 5
+})
+</script>
--- editeur_rust/rust-core/src/lib.rs
+++ editeur_rust/rust-core/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(left: u32, right: u32) -> u32 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
